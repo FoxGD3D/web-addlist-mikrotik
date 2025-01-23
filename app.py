f5e65aac694c1363432175b7426db84247ba7605
Игорь <fox@gd3d.ru>
2025-01-23 03:04:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import paramiko
 app = Flask(__name__)
 CORS(app)  # Allowing CORS requests
 
-# MikroTik configuration
+# Конфигурация MikroTik
 MIKROTIK_HOST = "Enter Mikrotik IP"
 MIKROTIK_USER = "Enter Mikrotik USER"
 MIKROTIK_PASSWORD = "Enter Mikrotik PASS"
@@ -17,7 +17,7 @@ def unlock_domain():
     domain = data.get("domain")
 
     if not domain:
-        return jsonify({"error": "Domain not specified"}), 400
+        return jsonify({"error": "Домен не указан"}), 400
 
     try:
         # Connecting to MikroTik via SSH
@@ -25,20 +25,20 @@ def unlock_domain():
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(MIKROTIK_HOST, port=MIKROTIK_PORT, username=MIKROTIK_USER, password=MIKROTIK_PASSWORD)
 
-        # Executing the command to add to the list
+        # Выполнение команды добавления в список
         command = f"/ip firewall address-list add list=rkn_wg address={domain} comment=AddFromApi"
         stdin, stdout, stderr = ssh.exec_command(command)
 
-        # Checking the result
+        # Проверка результата
         error = stderr.read().decode()
         if error:
-            return jsonify({"error": f"Command execution error: {error}"}), 500
+            return jsonify({"error": f"Ошибка выполнения команды: {error}"}), 500
 
         ssh.close()
-        return jsonify({"message": f"Domain {domain} successfully added to rkn_wg"})
+        return jsonify({"message": f"Домен {domain} успешно добавлен в rkn_wg"})
 
     except Exception as e:
-        return jsonify({"error": f"Connection error: {str(e)}"}), 500
+        return jsonify({"error": f"Ошибка при подключении: {str(e)}"}), 500
 
 
 if __name__ == '__main__':
